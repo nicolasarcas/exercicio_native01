4eb3ce6c56311c25792f4913ff46d81e7dd9bfe5
nicolasarcas <nicolas.arcas1@gmail.com>
2020-06-02 15:32:43 -0300

    final commit

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const[texto,setTexto] = useState("Números Aleatórios");
+  const[numero, setNumero] = useState(Math.ceil(Math.random() * 60));
+  const[contador, setContador] = useState(0);
+
+  var numerosAleatorios = [];
+   
+  const randomNumber = () =>{
+    
+    setNumero (Math.ceil(Math.random() * 60));
+
+    for(let i = 0; i<= numerosAleatorios.length;i++){
+      while(numero == numerosAleatorios[i]){
+        setNumero (Math.ceil(Math.random() * 60));
+      }
+    }
+    setContador(contador+1);
+    if(contador<6){
+      setTexto(""+numero);
+      numerosAleatorios.push(numero);
+    }
+    else{
+      setTexto("Numeros ja sorteados");
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{texto}</Text>
+      <View style={styles.botao}>
+        <Button
+          title="Rodar numero aleatorio"
+          onPress = {randomNumber}/>
+      </View>
     </View>
   );
 }
@@ -16,4 +45,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  botao:{
+    marginTop:4
+  }
 });
